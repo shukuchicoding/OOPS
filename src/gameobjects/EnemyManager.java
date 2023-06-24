7acc284954e46e92cd342e3b9c3dcfeb94d71a36
dodaophuc <dodaophuc1111@gmail.com>
2023-06-24 11:42:46 +0700

    Add UI

---
+++ src/gameobjects/EnemyManager.java
@@ -0,0 +1,69 @@
+package gameobjects;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import util.Resource;
+
+public class EnemyManager {
+	private BufferedImage tree;
+	private BufferedImage stone;
+	private Random rand;
+	
+	private List<Enemy> enemies;
+	private MainCharacter mainCharacter;
+	
+	public EnemyManager(MainCharacter mainCharacter) {
+		rand = new Random();
+		tree = Resource.getResourceImage("data/tree.png");
+		stone = Resource.getResourceImage("data/stone.png");
+		enemies = new ArrayList<Enemy>();
+		this.mainCharacter = mainCharacter;
+		enemies.add(createEnemy());
+	}
+	
+	public void update() {
+		for (Enemy e : enemies) {
+			e.update();
+		}
+	}
+	
+	public void draw(Graphics g) {
+		for (Enemy e : enemies) {
+			e.draw(g);
+		}
+		Enemy enemy = enemies.get(0);
+		if(enemy.isOutOfScreen()) {
+			mainCharacter.upScore();
+			enemies.clear();
+			enemies.add(createEnemy());
+		}
+	}
+	
+	private Enemy createEnemy() {
+		int type = rand.nextInt(2);
+		if (type == 0) {
+			return new TreeAndStone(mainCharacter, 600, tree.getWidth() - 10, tree.getHeight() - 10, tree);
+		}
+		else {
+			return new TreeAndStone(mainCharacter, 600, stone.getWidth() - 10, stone.getHeight() - 10, stone);
+		}
+	}
+	
+	public boolean isCollision() {
+		for (Enemy e : enemies) {
+			if (mainCharacter.getBound().intersects(e.getBound())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void reset () {
+		enemies.clear();
+		enemies.add(createEnemy());
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module OOPS {
+	requires java.desktop;
 }
